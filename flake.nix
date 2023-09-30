7cc5174ed13868d49eef1900e88e3c7157b112e4
Daniel Lambert <danjl1100@gmail.com>
2023-09-30 16:13:27 -0500

    demonstrate functional build using toml_edit branch of crane library

--- flake.nix
+++ flake.nix
@@ -3,7 +3,8 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     crane = {
-      url = "github:ipetkov/crane";
+      # url = "github:ipetkov/crane";
+      url = "github:danjl1100/crane?ref=toml_edit";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
